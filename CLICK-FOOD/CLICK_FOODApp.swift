fe690b14b8f4ba2e12856bbfcf76a34a981a3acd
Danilo Matheus Lopes de Oliveira <125399545+Danilo4009@users.noreply.github.com>
2025-05-26 19:49:57 -0300

    Initial Commit

---
+++ CLICK-FOOD/CLICK_FOODApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLICK_FOODApp.swift
+//  CLICK-FOOD
+//
+//  Created by Danilo Lopes on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CLICK_FOODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
